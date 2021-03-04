be95f2c5e469dc21654ff1690e92a84580d70405
Michael Lu <yeskky@gmail.com>
2021-03-04 16:58:03 +0800

    改行成MVC应用，注册服务，添加中间件

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace helloworld
 {
@@ -12,13 +13,17 @@ namespace helloworld
         {
             Host.CreateDefaultBuilder()
                 .ConfigureWebHostDefaults(webHostBuilder => webHostBuilder
+                    .ConfigureServices(services => services
+                        .AddRouting()
+                        .AddControllersWithViews())
                     .UseUrls("http://0.0.0.0:3721;https://0.0.0.0:9527")
                     // 注册处理请求的中间件
-                    .Configure(app => app.Run(
-                        context => context.Response.WriteAsync("Hello World.")
-                    )))
-                    .Build()
-                    .Run();
+                    .Configure(app => app
+                        .UseRouting()
+                        .UseEndpoints(endpoints=>endpoints.MapControllers()))
+                )
+                .Build()
+                .Run();
         }
     }
 }
