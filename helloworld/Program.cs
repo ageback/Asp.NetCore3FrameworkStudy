6c6935d72e5fdfa89968761e556a912f6ce47997
Michael Lu <yeskky@gmail.com>
2021-03-04 15:55:02 +0800

    修改为Asp.Net Core程序

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 
 namespace helloworld
 {
@@ -6,7 +10,16 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHost(webHostBuilder => webHostBuilder
+                    // 注册Kestrel为服务器
+                    .UseKestrel()
+                    // 注册处理请求的中间件
+                    .Configure(app => app.Run(
+                        context => context.Response.WriteAsync("Hello World.")
+                    )))
+                    .Build()
+                    .Run();
         }
     }
 }
